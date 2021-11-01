56d68a78831159819f05894e2a4d98ddab982458
Precious Abubakar <misspee007@gmail.com>
2021-11-01 19:02:51 +0100

    Updates

---
+++ precache-manifest.0a9811d86b6d6985863581c117259a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d6e8fdc4e5c8563cf22ae458386c93",
+    "url": "/markdown_editor/index.html"
+  },
+  {
+    "revision": "e017714c1b86747fcd9f",
+    "url": "/markdown_editor/static/css/main.775103e9.chunk.css"
+  },
+  {
+    "revision": "8a19ce2521fa0b573053",
+    "url": "/markdown_editor/static/js/2.56afce05.chunk.js"
+  },
+  {
+    "revision": "35285b9d7049ab43e8f7185465cacabe",
+    "url": "/markdown_editor/static/js/2.56afce05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e017714c1b86747fcd9f",
+    "url": "/markdown_editor/static/js/main.3bf8c9d2.chunk.js"
+  },
+  {
+    "revision": "bec2d2eaae04301883a6",
+    "url": "/markdown_editor/static/js/runtime-main.365de1e1.js"
+  }
+]);
